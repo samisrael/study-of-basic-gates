b1af6ca60c79004ae24583d15f218e2dbb22e52d
samisrael <samisrael0407@gmail.com>
2024-03-21 16:25:18 +0530

    final

--- GATES_TT.v
+++ GATES_TT.v
@@ -3,9 +3,10 @@ input a,b;
 output andgate,orgate,notgate,nandgate,norgate,xorgate,xnorgate;
 and(andgate,a,b);
 or(orgate,a,b);
-// type code for NOT Gate
+
 nand(nandgate,a,b);
 nor(norgate,a,b);
-// type code for XOR Gate
-// type code for XNOR Gate
+xor(xorgate,a,b);
+xnor(xnorgate,a,b);
+
 endmodule
